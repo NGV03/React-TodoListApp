c6032b0e27a21a48a96a09f25075b544c33c8c70
Neha Vekaria <nehavekaria@live.com>
2019-10-22 20:45:30 +1300

    design UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ToDoList from './ToDoList'
+import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <ToDoList/>
-      
+    <div className = "App">
+      <div className = "header">
+      <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className = "todo-list">
+      <ul>
+        <div className = "todo">
+          <div className = "checkbox"/>
+            <input type = "text" value = "To do one"/>
+          </div>
+  
+      </ul>
+    </form>
     </div>
   );
 }
