50cdf3cd3e5c23edf555db3fbe7daaa40b0fe9ff
Neha Vekaria <nehavekaria@live.com>
2019-10-23 17:54:44 +1300

    update and delete task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Shopping',
+      isCompleted: true,
+    },
+    {
+      content: 'Learn a new programming lang',
+      isCompleted: false,
+    }
+  ]);
+
+  function handleKeyDown (e, i){
+    if(e.key == 'Enter'){
+      createToDoAtIndex(e, i);
+    }
+    if(e.key == 'Backspace' && todos[i].content === ''){
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function createToDoAtIndex(e, i){
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0 ,{
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoAtIndex (e, i){
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
   return (
     <div className = "App">
       <div className = "header">
@@ -12,8 +59,19 @@ function App() {
       <form className = "todo-list">
       <ul>
         <div className = "todo">
-          <div className = "checkbox"/>
-            <input type = "text" value = "To do one"/>
+          <ul>
+            {todos.map((todo, i) =>(
+              <div className= "todo">
+                <div className = "checkbox"/>
+                <input 
+                type = "text" 
+                value = {todo.content} 
+                onKeyDown = {e => handleKeyDown (e,i)} 
+                onChange = {e => updateTodoAtIndex (e,i)}
+                />
+              </div>
+            ))}
+          </ul>
           </div>
   
       </ul>
